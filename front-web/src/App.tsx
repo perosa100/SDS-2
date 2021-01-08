84af7fafe3119f364de49c41949b3bf9dba77f3a
Patrick Perosa <patrickperosapp@gmail.com>
2021-01-08 00:20:42 -0300

    add navbar

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-web/src/NavBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg'
+
+const NavBar: React.FC = () => {
+  return (
+   <nav className="main-navbar">
+     <Logo />
+     <a href="home" className="logo-text">DS Delivery</a>
+   </nav>
+  );
+};
+
+export default NavBar;
